98a4b881be1e4edc09cbe422ef11de91f5a92009
akram <akramslimani277@gmail.com>
2024-11-28 14:48:53 +0100

    initial commit

---
+++ src/main/java/com/mabintech/mabintechapi/MabintechApiApplication.java
@@ -0,0 +1,13 @@
+package com.mabintech.mabintechapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabintechApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MabintechApiApplication.class, args);
+    }
+
+}
